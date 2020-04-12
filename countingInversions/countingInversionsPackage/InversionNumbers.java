c258dce57a24b101c281cd6728196c5a8933f59a
Nekesa Mercy <kesamercy@gmail.com>
2020-04-12 14:01:46 -0400

    initial commit

---
+++ countingInversions/countingInversionsPackage/InversionNumbers.java
@@ -0,0 +1,27 @@
+package countingInversionsPackage;
+
+public class InversionNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//read inputs from the terminal - n inputs -- how do we determine the end of the inputs  
+		
+		//the first line of input is 1 positive integer n btn 1 and 10^6
+		
+		//the A[1] - n input is the positive is btn 0 and 10^8.
+		
+		//the functions for the number of inversions
+		
+		//output the results to the terminal
+		
+		//this is the testing for the first commit message 
+		//testing for the inital git commit
+
+		
+		
+		//why do we need the pairs??? -- what are they for???
+		//can we used the n^2 algorithm for the counting inversions?
+	}
+
+}
